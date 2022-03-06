e571202fca7f1b20dea73e9d50a364e33227a316
lucabecci <beccibrian@gmail.com>
2022-03-06 15:01:51 -0300

    devops():scripts for prom and grap added

---
+++ scripts/run_graphana.sh
@@ -0,0 +1,8 @@
+exec(
+    docker run --rm -p 3000:3000 \
+    -e GF_AUTH_DISABLE_LOGIN_FORM=true \
+    -e GF_AUTH_ANONYMOUS_ENABLED=true \
+    -e GF_AUTH_ANONYMOUS_ORG_ROLE=Admin \
+    -v `pwd`/datasources.yml:/etc/grafana/provisioning/datasources/datasources.yml \
+    grafana/grafana:7.1.5
+)
