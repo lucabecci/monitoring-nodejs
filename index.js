fcd79afe0123bae69c58906c04be3203573997cb
lucabecci <beccibrian@gmail.com>
2022-03-06 13:51:25 -0300

    feat(): prometheus default metrics added

--- index.js
+++ index.js
@@ -2,19 +2,32 @@ require("dotenv").config()
 
 const express = require("express")
 const morgan = require("morgan")
+const client  = require("prom-client")
 
 const app = express()
+const register = new client.Registry()
+
+register.setDefaultLabels({
+    app: process.env.NAMESPACE
+})
+
+client.collectDefaultMetrics({register: register})
+
 app.use(morgan("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false, limit: "50mb" }))
 
 
-app.use('/v1/status', function(req, res){
+app.use('/ops/status', function(req, res){
     return res.status(200).json({
         namespace: process.env.NAMESPACE,
         port: process.env.PORT,
         env: process.env.NODE_ENV
     })
+})
+
+app.get("/ops/metrics", async function(req, res){
+    return res.end(await register.metrics())
 })
 
 app.listen(process.env.PORT, function(){
